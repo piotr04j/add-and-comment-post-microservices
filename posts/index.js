a946833058406a4b211eb25625f1ebbd7365dff5
piotr04j <darricck04@gmail.com>
2020-05-05 18:22:11 +0200

    fixed status in posts

--- posts/index.js
+++ posts/index.js
@@ -3,7 +3,7 @@ const { randomBytes } =  require('crypto')
 const bodyParser = require('body-parser')
 
 const app = express()
-app.use(bodyParser)
+app.use(bodyParser.json())
 
 const posts = {}
 
@@ -16,7 +16,7 @@ app.post('/posts', (req, res) => {
     const { title } = req.body
     posts[id] = title
 
-    res.status(200).send(posts[id])
+    res.status(201).send(posts[id])
 })
 
 

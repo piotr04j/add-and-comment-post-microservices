d0d4e4e04d174bef0491694b3c9fe278726fd731
piotr04j <darricck04@gmail.com>
2020-05-10 10:25:13 +0200

    added handling of comments moderation

--- comments/index.js
+++ comments/index.js
@@ -18,19 +18,33 @@ app.post('/posts/:id/comments', async (req, res) => {
     const id = randomBytes(4).toString('hex')
     const { content } = req.body
     const comments = commentsByPostId[req.params.id] || []
-    comments.push({ id, content })
+    comments.push({ id, content, status: 'pending' })
     commentsByPostId[req.params.id] = comments
 
     await axios.post('http://localhost:4005/events', {
         type: 'CommentCreated',
-        data:  { id, content, postId: req.params.id }
+        data:  { id, content, postId: req.params.id, status: 'pending' }
     })
 
     res.status(201).send(comments)
 })
 
-app.post('/events', (req, res) => {
-    console.log('Received event: ', req.body.type)
+app.post('/events', async (req, res) => {
+     const { type, data } = req.body
+
+    if ( type === 'CommentModerated') {
+        const { postId, id, status } = data
+        const comments = commentsByPostId[postId]
+        const comment = comments.find( comment => {
+            return comment.id === id
+        })
+        comment.status = status
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentUpdated',
+            data: { ...data, status }
+        })
+    }
 
     res.send({})
 })
